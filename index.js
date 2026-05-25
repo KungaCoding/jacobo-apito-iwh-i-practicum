72cc6e27be1237b36573c8712f6866cc82002afa
KungaCoding <kungacoding@gmail.com>
2026-05-24 23:54:58 -0400

    Add custom object routes and Pug templates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,19 +10,59 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const CUSTOM_OBJECT_URL = 'https://api.hubapi.com/crm/v3/objects/2-230139739';
+const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+};
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    try {
+        const resp = await axios.get(CUSTOM_OBJECT_URL, {
+            headers,
+            params: {
+                properties: 'name,element,power_level'
+            }
+        });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Objects | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to load custom object records.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            element: req.body.element,
+            power_level: req.body.power_level
+        }
+    };
+
+    try {
+        await axios.post(CUSTOM_OBJECT_URL, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to create custom object record.');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
